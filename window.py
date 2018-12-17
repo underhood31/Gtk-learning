ab3e1a38c09f4eeda64b6c63d03f6cbb17b560d5
underhood31 <manavjeet18295@iiitd.ac.in>
2018-12-17 22:51:29 +0530

    added file window.py -- to create basic windows

---
+++ window.py
@@ -0,0 +1,20 @@
+from gi.repository import Gtk
+import sys
+
+
+class MyApplication(Gtk.Application):
+
+    def do_activate(self):
+        # create a Gtk Window belonging to the application itself
+        window = Gtk.Window(application=self)
+        # set the title
+        window.set_title("Welcome to GNOME")
+        # show the window
+        window.set_default_size(1000, 800)
+        window.show_all()
+
+# create and run the application, exit with the value returned by
+# running the program
+app = MyApplication()
+exit_status = app.run(sys.argv)
+sys.exit(exit_status)
