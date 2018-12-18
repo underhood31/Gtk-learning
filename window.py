452d89210294e568bfeb45e4e749672d39064e47
underhood31 <manavjeet18295@iiitd.ac.in>
2018-12-18 10:05:13 +0530

    added applicationwindow

--- window.py
+++ window.py
@@ -10,7 +10,11 @@ class MyApplication(Gtk.Application):
         # set the title
         window.set_title("Welcome to GNOME")
         # show the window
-        window.set_default_size(1000, 800)
+        window.set_default_size(500, 400)
+        # set_position(Gtk.WindowPosition.CENTER) centers the window. 
+        # Other options are Gtk.WindowPosition.NONE, Gtk.WindowPosition.MOUSE, 
+        # Gtk.WindowPosition.CENTER_ALWAYS, Gtk.WindowPosition.CENTER_ON_PARENT.
+        window.set_position(Gtk.WindowPosition.CENTER_ON_PARENT)
         window.show_all()
 
 # create and run the application, exit with the value returned by
